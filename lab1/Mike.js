cb831642778cf23295af88649a484e487d25e466
Mike-Fig <59774939+Mike-Fig@users.noreply.github.com>
2020-01-18 16:18:55 -0500

    Add files via upload

---
+++ lab1/Mike.js
@@ -0,0 +1,9 @@
+const Person = require("./Person.js");
+
+class Mike extends Person {
+    constructor(name, color) {
+        super(name, color);
+    }
+}
+
+module.exports = Mike;
---
+++ lab1/index.js
@@ -0,0 +1,5 @@
+const Mike = require("./Mike.js");
+
+let p = new Mike("Mike", "blue");
+
+p.speak();
